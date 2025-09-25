ee92abe5726307242995d66eec5c058b9169ee40
Batiste Vancoillie <batiste.vancoillie@student.vives.be>
2025-09-25 12:01:32 +0200

    full stack les 1

---
+++ Introduction/Introduction/Controllers/PersonController.cs
@@ -0,0 +1,44 @@
+﻿using Introduction.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Introduction.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //get-request
+        //default get-request, dus dit moet je niet schrijven [HttpGet]
+        public IActionResult Register()
+        {
+            //Commando's
+            return View();
+        }
+
+        [HttpPost] //enkel via een submit geraak je hierin
+        public IActionResult Register(string txtName, string txtFirstName)
+        {
+            //Commando's
+            return View();
+        }
+
+
+        public IActionResult RegisterNew()
+        {
+            //Commando's
+            return View();
+        }
+
+        [HttpPost] //enkel via een submit geraak je hierin
+        public IActionResult RegisterNew(RegisterVM personVM)
+        {
+            //Commando's
+            return View();
+        }
+
+
+    }
+}
